249661dbe755b793802a600e0ce02463b9cafbf3
Alex Tokarev <tokarev@cpan.org>
2020-03-17 15:03:04 -0700

    Add error boundary component

--- src/index.js
+++ src/index.js
@@ -3,18 +3,21 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ViewModel from 'statium';
 
+import ErrorBoundary from './ErrorBoundary.js';
 import App from './App.js';
 
 import './index.css';
 
 const Index = () => (
-    <ViewModel id="index" data={{ appName: 'Conduit' }}>
-        <BrowserRouter>
-            <Switch>
-                <Route path="/" component={App} />
-            </Switch>
-        </BrowserRouter>
-    </ViewModel>
+    <ErrorBoundary>
+        <ViewModel id="index" data={{ appName: 'Conduit' }}>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/" component={App} />
+                </Switch>
+            </BrowserRouter>
+        </ViewModel>
+    </ErrorBoundary>
 );
 
 ReactDOM.render(<Index />, document.getElementById('root'));
